07cdbb48f5394f082d5765bdb913ac4aa3bd50a2
Pombur <125867631+Pombur@users.noreply.github.com>
2023-03-08 00:30:42 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
-var YANDEX_API_KEY = '94bb227a-f063-4544-a825-3141b21ccfc6';
+var config = {
+    yandexApiKey: '94bb227a-f063-4544-a825-3141b21ccfc6',
+    googleApiKey: 'AIzaSyBuQc7Oy5xHOgbdIkrb1omZpEF95orEqYs'
+};
+
 
